b070fca8aee0a463788bc6d4b6a76aba5c35b2cd
Bui Gia An <buigiaanfb1@gmail.com>
2021-11-21 20:25:09 +0700

    missing component folder

---
+++ src/context/BlogContext.js
@@ -0,0 +1,61 @@
+import createDataContext from './createDataContext';
+
+const blogReducer = (state, action) => {
+  switch (action.type) {
+    case 'add_blogpost':
+      return [
+        ...state,
+        {
+          id: Math.floor(Math.random() * 9999),
+          title: action.payload.title,
+          content: action.payload.content,
+        },
+      ];
+
+    case 'delete_blogpost':
+      return state.filter((blogPost) => blogPost.id !== action.payload);
+
+    case 'edit_blogpost':
+      return state.map((blogPost) => {
+        return blogPost.id === action.payload.id ? action.payload : blogPost;
+      });
+
+    default:
+      return state;
+  }
+};
+
+const addBlogPost = (dispatch) => {
+  return (title, content, callback) => {
+    dispatch({
+      type: 'add_blogpost',
+      payload: { title, content },
+    });
+    callback();
+  };
+};
+
+const deleteBlogPost = (dispatch) => {
+  return (id) => {
+    dispatch({
+      type: 'delete_blogpost',
+      payload: id,
+    });
+  };
+};
+
+const editBlogPost = (dispatch) => {
+  return (id, title, content, callback) => {
+    dispatch({
+      type: 'edit_blogpost',
+      payload: { id, title, content },
+    });
+    callback();
+  };
+};
+
+export const { Context, Provider } = createDataContext(
+  blogReducer,
+  { addBlogPost, deleteBlogPost, editBlogPost },
+  [{ title: 'Test', content: 'test', id: 1 }]
+);
